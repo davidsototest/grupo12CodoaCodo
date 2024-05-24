873fb193da9dd9bf017de5f4e75f5f52e892ccbc
zaratenicolasm <zaratenicolasm@gmail.com>
2024-05-23 22:26:08 -0300

    Validaciones en el form y modificaciones de imagenes

--- TrabajoFinal/script.js
+++ TrabajoFinal/script.js
@@ -33,3 +33,103 @@ document.querySelectorAll('#homeButton, #publicationsButton, #contactButton, #co
     });
 });
 
+//    ---------- Validación de formulario -----------
+if(document.querySelector('#form') !== null){
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("#form").addEventListener('submit', validarFormulario); 
+});
+}
+
+function validarFormulario(evento) {
+    evento.preventDefault();   
+    let formulario = document.querySelector('#form')
+    let listaErrores = document.querySelector('#errores')
+    listaErrores.innerHTML = "";
+    //Nombre
+    let nombre = document.querySelector('#nombre').value;
+    if(validarRango(nombre.length, 3, 20)) {
+      listaErrores.innerHTML += "*Nombre: entre 3 y 20 carácteres."
+      return;
+    }
+    if(!validarString('simple', nombre)){
+        listaErrores.innerHTML += "*Nombre: solo letras."
+        return;
+    }
+    //Apellido
+    let apellido = document.querySelector('#apellido').value;
+    if (validarRango(apellido.length, 3, 20)) {
+        listaErrores.innerHTML += "*Apellido: entre 3 y 20 carácteres."
+      return;
+    }
+    if(!validarString('simple', apellido)){
+        listaErrores.innerHTML += "*Apellido: solo letras."
+        return;
+    }
+    //E-mail
+    let correo = document.querySelector('#correo').value;
+    if(!validarString('email', correo)){
+        listaErrores.innerHTML += "*Ingrese un email valido."
+        return;
+    }
+    //telefono
+    let telefono = document.querySelector('#telefono').value;
+    if(validarRango(telefono, 1000000000, 99999999999)){
+        listaErrores.innerHTML += "*Ingrese un numero valido (con cod. área)."
+        return;
+    }
+    //provincia
+    let provincia = document.querySelector('#provincia').value;
+    if (validarRango(provincia.length, 3, 35)) {
+        listaErrores.innerHTML += "*Provincia: entre 3 y 35 carácteres."
+        return;
+    }
+    if(!validarString('compuesto', provincia)){
+        listaErrores.innerHTML += "*Provincia: solo letras y números."
+        return;
+    }
+    //ciudad
+    let ciudad = document.querySelector('#ciudad').value;
+    if (validarRango(ciudad.length, 3, 35)) {
+        listaErrores.innerHTML += "*Ciudad: entre 3 y 35 carácteres."
+        return;
+    }  
+    if(!validarString('compuesto', ciudad)){
+        listaErrores.innerHTML += "*Ciudad: solo letras y números."
+        return;
+    }
+
+    listaErrores.className = "font-style-normalText-italic success double"
+    listaErrores.innerHTML = "¡Gracias por comunicarse con nosotros!"
+    
+    //Submit
+    setTimeout(function(){
+        formulario.submit()
+    }, 1500);
+    
+}
+
+function validarRango(valor, min, max){
+    if(valor < min || valor > max){
+        return true
+    }
+    return false
+}
+
+function validarString(tipoDeString, texto){
+    let valido = true;
+    switch(tipoDeString){
+        case('simple'):
+            let palabraSimple = new RegExp('^[A-Z\u00f1\u00d1\u00E0-\u00FC]+$', 'i');
+            valido = palabraSimple.test(texto);
+            break;
+        case('compuesto'):
+            let palabraCompuesta = new RegExp('^[A-Z0-9 \u00f1\u00d1\u00E0-\u00FC]+$', 'i');
+            valido = palabraCompuesta.test(texto);
+            break;
+        case('email'):
+            let emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+            valido = emailRegex.test(texto);
+            break;
+    }
+    return valido;
+}
